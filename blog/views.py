9a4bbf21022e84b476c1365119c83c28f673475d
haoxiang <haoxiang@tencent.com>
2021-08-24 16:40:01 +0800

    update

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from .models import Post, Category, Tag
 
 
 def index(request):
-    post_list = Post.objects.all().order_by('-created_time')
+    post_list = Post.objects.all()
     return render(request, 'blog/index.html', context={
         'post_list': post_list
     })
@@ -19,7 +19,7 @@ def archive(request, year, month):
     post_list = Post.objects.filter(
         created_time__year=year,
         created_time__month=month
-    ).order_by('-created_time')
+    )
     return render(request, 'blog/index.html', context={
         'post_list': post_list
     })
@@ -29,7 +29,7 @@ def category(request, pk):
     cate = get_object_or_404(Category, pk=pk)
     post_list = Post.objects.filter(
         category=cate
-    ).order_by('-created_time')
+    )
     return render(request, 'blog/index.html', context={
         'post_list': post_list
     })
@@ -39,7 +39,7 @@ def tag(request, pk):
     t = get_object_or_404(Tag, pk=pk)
     post_list = Post.objects.filter(
         tag=t
-    ).order_by('-created_time')
+    )
     return render(request, 'blog/index.html', context={
         'post_list': post_list
     })
