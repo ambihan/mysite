9c2eb7548c60bb0905c60ac113fbf0aef7cc449d
haoxiang <haoxiang@tencent.com>
2021-08-27 17:38:39 +0800

    docker deploy

--- djangoProject/settings/production.py
+++ djangoProject/settings/production.py
@@ -11,7 +11,7 @@ production
 from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a(f*&uwl)qgfhw1^=*c&bby#dnjgqs(1mwa%tzs=w_ywl133(n'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
