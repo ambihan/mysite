54bb55923e336f3de64bf13c2448de08d800bc65
haoxiang <haoxiang@tencent.com>
2021-08-27 18:40:07 +0800

    allow all

--- djangoProject/settings/production.py
+++ djangoProject/settings/production.py
@@ -16,4 +16,4 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.smoba.cn']
+ALLOWED_HOSTS = ['*']
