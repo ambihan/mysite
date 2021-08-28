bfe076b991e354a6a08352895ffdb72202d3d53f
haoxiang <haoxiang@tencent.com>
2021-08-28 21:29:06 +0800

    类视图

--- blog/urls.py
+++ blog/urls.py
@@ -14,12 +14,12 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:pk>/', views.detail, name='detail'),
-    path('archive/<int:year>/<int:month>', views.archive, name='archive'),
-    path('category/<int:pk>/', views.category, name='category'),
-    path('tag/<int:pk>/', views.tag, name='tag'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('posts', views.PostView.as_view(), name='posts'),
+    path('archive/<int:year>/<int:month>', views.ArchiveView.as_view(), name='archive'),
+    path('category/<int:pk>/', views.CategoryView.as_view(), name='category'),
+    path('tag/<int:pk>/', views.TagView.as_view(), name='tag'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
-    path('posts', views.posts, name='posts'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,65 +2,79 @@ import markdown
 from markdown.extensions.toc import TocExtension
 from django.utils.text import slugify
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Post, Category, Tag
 
 
 # Create your views here.
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
 
-def index(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/index.html', context={
-        'post_list': post_list
-    })
+class PostView(ListView):
+    model = Post
+    template_name = 'blog/posts.html'
+    context_object_name = 'post_list'
 
 
-def archive(request, year, month):
-    post_list = Post.objects.filter(
-        created_time__year=year,
-        created_time__month=month
-    )
-    return render(request, 'blog/index.html', context={
-        'post_list': post_list
-    })
+class CategoryView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
 
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(
-        category=cate
-    )
-    return render(request, 'blog/index.html', context={
-        'post_list': post_list
-    })
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        t = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=t)
 
 
-def tag(request, pk):
-    t = get_object_or_404(Tag, pk=pk)
-    post_list = Post.objects.filter(
-        tag=t
-    )
-    return render(request, 'blog/index.html', context={
-        'post_list': post_list
-    })
+class ArchiveView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        return super(ArchiveView, self).get_queryset().filter(
+            created_time__year=self.kwargs.get('year'),
+            created_time__month=self.kwargs.get('month')
+        )
 
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.increase_views()
-    md = markdown.Markdown(
-        extensions=[
-            'markdown.extensions.extra',
-            'markdown.extensions.codehilite',
-            TocExtension(slugify=slugify),
-        ]
-    )
-    post.body = md.convert(post.body)
-    post.toc = md.toc
-    post.has_toc = '<li>' in post.toc
-    return render(request, 'blog/detail.html', context={
-        'post': post
-    })
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        md = markdown.Markdown(
+            extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+                TocExtension(slugify=slugify),
+            ]
+        )
+        post.body = md.convert(post.body)
+        post.toc = md.toc
+        post.has_toc = '<li>' in post.toc
+
+        return post
 
 
 def about(request):
@@ -69,10 +83,3 @@ def about(request):
 
 def contact(request):
     return render(request, 'blog/contact.html')
-
-
-def posts(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/posts.html', context={
-        'post_list': post_list
-    })
