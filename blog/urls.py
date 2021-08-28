79e522cdc7ed9b0e6c34c9b0c4e13949133d3792
haoxiang <haoxiang@tencent.com>
2021-08-28 23:08:09 +0800

    update

--- blog/urls.py
+++ blog/urls.py
@@ -15,11 +15,11 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('posts', views.PostView.as_view(), name='posts'),
-    path('archive/<int:year>/<int:month>', views.ArchiveView.as_view(), name='archive'),
+    path('posts/', views.PostView.as_view(), name='posts'),
+    path('archive/<int:year>/<int:month>/', views.ArchiveView.as_view(), name='archive'),
     path('category/<int:pk>/', views.CategoryView.as_view(), name='category'),
     path('tag/<int:pk>/', views.TagView.as_view(), name='tag'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
-    path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
